b1c3075d873e718ac4555b6dfdd6e5fac0f0ee50
TAMIL PRIYAN N <tamil.n2777@gmail.com>
2025-11-14 20:56:43 +0530

    file renamed

--- basic_programs/p02_odd_even.py
+++
@@ -1,5 +0,0 @@
-a=5
-if a%2==0:
-    print(a,"is even")
-else:
-    print(a,"is odd")
---
+++ basic_programs/p03_odd_even.py
@@ -0,0 +1,10 @@
+a=5
+if a%2==0:
+    print(a,"is even")
+else:
+    print(a,"is odd")
+b=int(input("enter a number:"))
+if b%2==0:
+    print(b,"is even")
+else:
+    print(b,"is odd")
