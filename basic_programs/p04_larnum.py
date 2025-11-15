bd7276228d4360823bd168bec0927d0c9f74310c
TAMIL PRIYAN N <tamil.n2777@gmail.com>
2025-11-15 21:01:06 +0530

    Update p04_larnum.py

--- basic_programs/p04_larnum.py
+++ basic_programs/p04_larnum.py
@@ -7,3 +7,14 @@ elif b>c and b>a:
     print(b,"is largest")
 else:
     print(c,"is largest")
+
+x=int(input("enter a number 1:"))
+y=int(input("enter a number 2:"))
+z=int(input("enter a number 3:"))
+
+if x>y and x>z:
+    print(x,"is largest")
+elif y>z:
+    print(y,"is largest")
+else:
+    print(z,"is largest")
