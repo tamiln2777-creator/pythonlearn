3f93e1edc131f577d86682a9cb422ca9bb2bc858
TAMIL PRIYAN N <tamil.n2777@gmail.com>
2026-03-14 19:06:54 +0530

    Delete 46_string_compression.py

--- 3_hard_programs/46_string_compression.py
+++
@@ -1,8 +0,0 @@
-s=input("enter a string:")
-count=1
-for i in range(len(s)):
-    if i+1<len(s) and s[i]==s[i+1]:
-        count+=1
-    else:
-        print(s[i]+str(count),end="")
-        count=1
